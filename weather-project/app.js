c9c5f9bcba20b72caaac3df48e9451dffd79bd01
jasper <63331153+jasper-oh@users.noreply.github.com>
2021-01-29 20:32:42 +0900

    weather web-app

---
+++ weather-project/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const https = require("https");
+
+const app = express();
+
+app.get("/", function (req, res) {
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=seoul&units=metric&appid=b3cb5d7584bd68d247a77ece4ca7d623";
+  https.get(url, function (response) {
+    console.log(response.statusCode);
+
+    response.on("data", function (data) {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const weatherDescription = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+      res.write("<p>The weather is currently" + weatherDescription + "</p>");
+      res.write(
+        "<h1>The temperature in Seoul is " + temp + " degrees Celcius</h1>"
+      );
+      res.write("<img src=" + imageURL + ">");
+      res.send();
+    });
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Server is running on port 3000.");
+});
